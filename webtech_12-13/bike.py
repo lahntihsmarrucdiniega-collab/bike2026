d6cfca3c7784e0fbc83675c35fc7500e90736e61
Diniega Lahntihs marru c. <lahntihsmarrucdiniega@gmail.com>
2026-01-14 23:40:34 +0800

    Update bike.py

--- webtech_12-13/bike.py
+++ webtech_12-13/bike.py
@@ -121,10 +121,16 @@ def get_data():
         print(f"Error occurred: {e}")
         return "Something went wrong internally.", 500
 
+@app.errorhandler(500)
+def internal_error(error):
+    # This function runs whenever a 500 error happens
+    return render_template('500.html'), 500
+
 # ---------- MAIN ----------
 if __name__ == "__main__":
     # Change debug=True to see errors in the browser
     app.run(debug=True)
+
 
 
 
